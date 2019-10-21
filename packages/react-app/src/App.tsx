32fec66d12293fee8aafca05e7301e7b5462ef08
Michal Jach <michal.jach@grandparade.co.uk>
2019-10-21 12:45:15 +0200

    init

---
+++ packages/react-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@namespace/components";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+};
+
+export default App;
